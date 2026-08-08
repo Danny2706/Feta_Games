0717a0d7c863b015d7fffcf64e03d802935d26d7
Danny2706 <danikume27@gmail.com>
2026-08-09 00:22:35 +0300

    slice

--- feta_games-main/client/src/App.tsx
+++ feta_games-main/client/src/App.tsx
@@ -1,95 +1,95 @@
-// import React, { Suspense, useEffect, useState } from "react";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Game from "./pages/player/Game";
-// import Invite from "./pages/player/Invite";
-// import Mainlayout from "./layout/Mainlayout";
-// import Profile from "./pages/player/Profile";
-// // import ConnectFour from "./pages/game/connectfour/ConnectFour";
-// // import ConnectFourMatchmaking from "./pages/game/connectfour/ConnectFourMatchmaking";
-// import Jetx from "./pages/game/Jetx/Jetx";
-// import MemoryFlip from "./pages/game/memoryflip/MemoryFlip";
-// import MinesDuel from "./pages/game/minesduel/MinesDuel";
-// import CardDraw from "./pages/game/carddraw/CardDraw";
-// import CardDrawMatchmaking from "./pages/game/carddraw/CardDrawMatchmaking";
-// import { ToastContainer } from "react-toastify"
-// import { initAuth } from "./store/slice/auth";
-// import { useAppDispatch } from "./store/hook";
-// import { connectSocket } from "./lib/socket";
-// import { toast } from 'react-toastify';
-// import Deposit from "./pages/player/Deposit";
-// import MatchMakingLayout from "./layout/MatchMakingLayout";
-// import { registerSocketListeners } from "./lib/socketListeners";
-// import MinimalLayout from "./layout/MinimalLayout";
-// import Loading from "./components/layout/Loading";
+import React, { Suspense, useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Game from "./pages/player/Game";
+import Invite from "./pages/player/Invite";
+import Mainlayout from "./layout/Mainlayout";
+import Profile from "./pages/player/Profile";
+// import ConnectFour from "./pages/game/connectfour/ConnectFour";
+// import ConnectFourMatchmaking from "./pages/game/connectfour/ConnectFourMatchmaking";
+import Jetx from "./pages/game/Jetx/Jetx";
+import MemoryFlip from "./pages/game/memoryflip/MemoryFlip";
+import MinesDuel from "./pages/game/minesduel/MinesDuel";
+import CardDraw from "./pages/game/carddraw/CardDraw";
+import CardDrawMatchmaking from "./pages/game/carddraw/CardDrawMatchmaking";
+import { ToastContainer } from "react-toastify"
+import { initAuth } from "./store/slice/auth";
+import { useAppDispatch } from "./store/hook";
+import { connectSocket } from "./lib/socket";
+import { toast } from 'react-toastify';
+import Deposit from "./pages/player/Deposit";
+import MatchMakingLayout from "./layout/MatchMakingLayout";
+import { registerSocketListeners } from "./lib/socketListeners";
+import MinimalLayout from "./layout/MinimalLayout";
+import Loading from "./components/layout/Loading";
 
 
-// export default function App() {
-//   const dispatch = useAppDispatch();
-//   const [ready, setReady] = useState(false);
-//   useEffect(() => {
-//     async function init() {
-//       try {
-//         await dispatch(initAuth()).unwrap();
-//         await connectSocket();
-//         await registerSocketListeners();
-//       } catch (err: unknown) {
-//         let message = "Initialization failed";
-//         if (err instanceof Error) {
-//           message = err.message;
-//         }
-//         toast.error(message);
-//       } finally {
-//         setReady(true);
-//       }
-//     }
+export default function App() {
+  const dispatch = useAppDispatch();
+  const [ready, setReady] = useState(false);
+  useEffect(() => {
+    async function init() {
+      try {
+        await dispatch(initAuth()).unwrap();
+        await connectSocket();
+        await registerSocketListeners();
+      } catch (err: unknown) {
+        let message = "Initialization failed";
+        if (err instanceof Error) {
+          message = err.message;
+        }
+        toast.error(message);
+      } finally {
+        setReady(true);
+      }
+    }
 
-//     init();
-//   });
+    init();
+  });
 
 
-//   if (!ready) {
-//     return (
-//       <Loading />
-//     );
-//   }
+  if (!ready) {
+    return (
+      <Loading />
+    );
+  }
 
-//   return (
-//     <Suspense fallback={<Loading />}>
+  return (
+    <Suspense fallback={<Loading />}>
 
-//       <BrowserRouter>
-//         <Routes>
-//           <Route element={<Mainlayout />}>
-//             <Route index element={<Game />} />
-//             <Route path="/profile" element={<Profile />} />
-//             <Route path="/invite" element={<Invite />} />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Mainlayout />}>
+            <Route index element={<Game />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/invite" element={<Invite />} />
 
-//           </Route>
-//           <Route element={<MatchMakingLayout />}>
-//             {/* <Route path="/connectfour" element={<ConnectFourMatchmaking />} /> */}
-//             <Route path="/carddraw" element={<CardDrawMatchmaking />} />
-//             <Route path="/jetxpick" element={<Jetx />} />
-//             <Route path="/memoryflip" element={<MemoryFlip />} />
-//             <Route path="/minesduel" element={<MinesDuel />} />
-//           </Route>
+          </Route>
+          <Route element={<MatchMakingLayout />}>
+            {/* <Route path="/connectfour" element={<ConnectFourMatchmaking />} /> */}
+            <Route path="/carddraw" element={<CardDrawMatchmaking />} />
+            <Route path="/jetxpick" element={<Jetx />} />
+            <Route path="/memoryflip" element={<MemoryFlip />} />
+            <Route path="/minesduel" element={<MinesDuel />} />
+          </Route>
 
-//           <Route element={<MinimalLayout />}>
-//             <Route path="/deposit/:trxno" element={<Deposit />} />
-//           </Route>
-//           {/* <Route path="/connectfour/:roomId" element={<ConnectFour />} /> */}
-//           <Route path="/carddraw/:roomId" element={<CardDraw />} />
-//         </Routes>
+          <Route element={<MinimalLayout />}>
+            <Route path="/deposit/:trxno" element={<Deposit />} />
+          </Route>
+          {/* <Route path="/connectfour/:roomId" element={<ConnectFour />} /> */}
+          <Route path="/carddraw/:roomId" element={<CardDraw />} />
+        </Routes>
 
-//         <ToastContainer
-//           position="top-center"
-//           autoClose={2000}
-//           hideProgressBar
-//           newestOnTop
-//           closeOnClick
-//           draggable={false}
-//           pauseOnHover={false}
-//           theme="dark"
-//         />
-//       </BrowserRouter>
-//     </Suspense>
-//   );
-// }
+        <ToastContainer
+          position="top-center"
+          autoClose={2000}
+          hideProgressBar
+          newestOnTop
+          closeOnClick
+          draggable={false}
+          pauseOnHover={false}
+          theme="dark"
+        />
+      </BrowserRouter>
+    </Suspense>
+  );
+}
