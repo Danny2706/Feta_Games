787be721ff5c7a529ce09f7cc845a1cc89c80183
Danny2706 <danikume27@gmail.com>
2026-08-10 14:36:51 +0300

    wallet route

--- feta_games-main/server/src/routes/wallet.route.ts
+++ feta_games-main/server/src/routes/wallet.route.ts
@@ -1,22 +1,22 @@
-import express from "express";
-import {
-  deposit,
-  wallet,
-  paymentMethod,
-  withDraw,
-  transactions,
-  getTransaction,
-  getWithdraws,
-} from "../controller/wallet.controller";
-import { requireAuth } from "../middleware/auth";
-const route = express.Router();
-route.use(requireAuth);
-route.get("/", wallet);
-route.post("/paymentmethod", paymentMethod);
-route.post("/deposit", deposit);
-route.post("/withdraw", withDraw);
-route.get("/withdraw", getWithdraws);
-route.get("/transactions", transactions);
-route.get("/gettransaction/:trxno", getTransaction);
+// import express from "express";
+// import {
+//   deposit,
+//   wallet,
+//   paymentMethod,
+//   withDraw,
+//   transactions,
+//   getTransaction,
+//   getWithdraws,
+// } from "../controller/wallet.controller";
+// import { requireAuth } from "../middleware/auth";
+// const route = express.Router();
+// route.use(requireAuth);
+// route.get("/", wallet);
+// route.post("/paymentmethod", paymentMethod);
+// route.post("/deposit", deposit);
+// route.post("/withdraw", withDraw);
+// route.get("/withdraw", getWithdraws);
+// route.get("/transactions", transactions);
+// route.get("/gettransaction/:trxno", getTransaction);
 
-export default route;
+// export default route;
